768928b617284238e39ccf019ef3c68d92517cd9
Dominik Haeflinger <domhaeflinger@gmail.com>
2016-10-29 15:20:40 -0500

    Added enums and block class

---
+++ Qwirkle/Qwirkle/Block.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qwirkle
+{
+    public class Block
+    {
+        private BlockColor _color;
+        private BlockShape _shape;
+
+        public BlockShape Shape
+        {
+            get { return _shape; }
+            set { _shape = value; }
+        }
+
+        public BlockColor Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public Block(BlockColor color, BlockShape shape)
+        {
+            _color = color;
+            _shape = shape;
+        }
+
+        public Image GetImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
